4b6b0571632232d861dda0a8b291205f0cc33bd9
Ankitpal23 <129163661+Ankitpal23@users.noreply.github.com>
2024-01-11 22:44:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,5 @@
-let inputbox=document.getElementById("input-box");
-let listcontainer=document.getElementById("list-container");
-function addTask(){
-    if(inputbox.value == ""){
-        alert("You must write something");
-    }
-    else{
-        let li= document.createElement("li");
-        li.innerHTML =inputbox.value;
-        listcontainer.appendChild(li);
-        let span =document.createElement("span");
-        span.innerHTML ="\u00d7";
-        li.appendChild(span);
-    }
-    inputbox.value="" ;
-    savedata();
-}
-listcontainer.addEventListener("click",function(e)
-{
-    if(e.target.tagName == "Li"){
-        e.target.classList.toggle("checked");
-    }
-    else if(e.target.tagName == "span"){
-        e.target.parentElement.remove();
-        savedata();
-    }
-},false);
+let subMenu = document.getElementById("subMenu");
 
-function savedata(){
-    localStorage.setItem("data",listcontainer.innerHTML);
+function toggleMenu(){
+    subMenu.classList.toggle("open-menu");
 }
-functionshowTask()
-    {listcontainer.innerHTML=localStorage.getItem("data");
-
-}
-showTask();
